bf9e128564d5058324f3a05af40e8fc893e28fca
dxjester <patbenitez@gmail.com>
2017-10-12 11:58:33 -0700

    Create ifElse.py

---
+++ ifElse.py
@@ -0,0 +1,19 @@
+'''
+Program Title: ifElse.py 
+Date created: 12 OCT 17 
+Author: dxjester21
+'''
+
+# Make sure that the_flying_circus() returns True
+a = 5
+b = 6
+c = 7
+
+def the_flying_circus():
+    if (a+b >7 and a <7):    # Start coding here!
+        return True
+    elif (a+b < 7 and b < 7):
+        return False
+    else: 
+    		return False
+    	
